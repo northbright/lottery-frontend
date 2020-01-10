bbac604547abbbd32026a1826286f3c8249f23e9
Frank Xu <frank.xu@northbright.com>
2020-01-10 19:12:53 +0800

    Disable no-console rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
 
     // allow debugger during development only
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off"
+    //"no-console": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-console": "off"
   }
 };
